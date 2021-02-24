80274985c23bbf29a9a3e6dc4b00f9319d19863a
Alejandro Arcila Mora <alejo0618@gmail.com>
2021-02-24 22:19:38 +1100

    Fix: docker build now receives env variables

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
+const favoriteFood = process.env.FAVORITE_FOOD;
 
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 async function main() {
-  while(true) {
-    console.log("Microservices rock!");
-    const favoriteFood = process.env.FAVORITE_FOOD;
+  while(true) {       
     console.log(`My favorite food is ${favoriteFood}`);
     await sleep(5000);
   }
