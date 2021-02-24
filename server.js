2aeb56387079aa31ca6143f10c43c621bde52df6
Alejandro Arcila Mora <alejo0618@gmail.com>
2021-02-24 21:53:18 +1100

    chore: Travis file removed from dev environment

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
+
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 async function main() {
   while(true) {
-    console.log('Containers rule!');
+    console.log("Microservices rock!");
+    const favoriteFood = process.env.FAVORITE_FOOD;
+    console.log(`My favorite food is ${favoriteFood}`);
     await sleep(5000);
   }
 }
